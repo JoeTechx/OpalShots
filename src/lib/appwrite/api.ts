813089fd52a55d7e194505b6e6625cb67a5d7bf8
JOEX <oyinjoe23@gmail.com>
2023-12-07 07:21:27 +0100

    feat: Explore pages, search api and infinit scroll functionality and api

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -248,20 +248,18 @@ export async function deleteSavedPost(savedRecordId: string) {
   }
 }
 
-export async function getPostById(postId: string){
+export async function getPostById(postId: string) {
   try {
     const post = await databases.getDocument(
       appwriteConfig.databaseId,
       appwriteConfig.postCollectionId,
-      postId,
-    )
-    return post
+      postId
+    );
+    return post;
   } catch (error) {
     console.log(error);
-    
   }
 }
-
 
 // ============================== UPDATE POST
 export async function updatePost(post: IUpdatePost) {
@@ -347,3 +345,40 @@ export async function deletePost(postId?: string, imageId?: string) {
     console.log(error);
   }
 }
+
+export async function getInfinitePosts({ pageParam }: { pageParam: number }) {
+  const queries: any[] = [Query.orderDesc("$updatedAt"), Query.limit(9)];
+
+  if (pageParam) {
+    queries.push(Query.cursorAfter(pageParam.toString()));
+  }
+
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      queries
+    );
+    if (!posts) throw Error;
+
+    return posts;
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function searchPosts(searchTerm: string) {
+  try {
+    const post = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+   [Query.search('caption', searchTerm)]
+    );
+    if (post) throw Error;
+    return post;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
--- src/lib/react-query/queryAndMutations.ts
+++ src/lib/react-query/queryAndMutations.ts
@@ -10,10 +10,12 @@ import {
   deletePost,
   deleteSavedPost,
   getCurrentUser,
+  getInfinitePosts,
   getPostById,
   getRecentPosts,
   likePost,
   savePost,
+  searchPosts,
   signInAccount,
   signOutAccount,
   updatePost,
@@ -165,6 +167,29 @@ export const useDeletePost = () => {
     },
   });
 };
+
+export const useGetPosts = ()  => {
+return useInfiniteQuery({
+  queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+  queryFn: getInfinitePosts,
+  getNextPageParam: (lastPage) => {
+    if(lastPage && lastPage.documents.length === 0) return null
+    const lastId = lastPage.documents[lastPage?.documents.length - 1].$id;
+    return lastId;
+  }
+})
+
+}
+
+
+export const useSearchPosts = (searchTerm: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.SEARCH_POSTS, searchTerm],
+    queryFn: () => searchPosts(searchTerm),
+    enabled: !!searchTerm,
+  });
+};
+
 // ============================================================
 // USER QUERIES
 // ============================================================
